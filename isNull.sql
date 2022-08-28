47076ecad8926ae717e3fe2d0b9dfaaa65e9b37b
ibrahim <bcelikel1994@gmail.com>
2022-08-28 18:28:13 +0300

    28-08-2022

--- isNull.sql
+++ isNull.sql
@@ -19,3 +19,36 @@ INSERT INTO insanlar (ssn) VALUES('999111222');
 select * from insanlar
 -- isim i null olanlari sorgula
 SELECT * from insanlar where isim is null
+-- null olmayanları listeleyelim
+select * from insanlar WHERE isim is not null
+-- isim 'i NULL olan kişilerin isim'ine NO NAME atayınız.
+UPDATE insanlar
+SET isim = 'NO NAME'
+WHERE isim is null
+-- Tabloyu bir önceki soruyu çözmeden önceki haline geri getirin
+UPDATE insanlar
+SET isim = null
+WHERE isim = 'NO NAME'
+--NOT : Çoklu değişimde her değişim için SET ... WHERE isim IS NULL gibi 
+--ifade yazmamak için COALESCE(Birleştirme) kullanılır 
+/* 
+isim 'i NULL olanlara 'Henuz isim girilmedi'
+adres 'i NULL olanlara 'Henuz adres girilmedi'
+ssn 'i NULL olanlara ' no ssn' atayalım.
+*/
+
+update insanlar
+set isim = coalesce (isim, 'HENUZ ISIM GIRILMEDI'),
+adres= coalesce (adres, 'HENUZ ADRES GIRILMEDI'),
+ssn=coalesce (ssn,'NO SSN')
+
+
+
+
+
+
+
+
+
+
+
